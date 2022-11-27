14ea64a025a36d9cb3accb717f5d810f00e46a4d
GribanovDaniil <113581434+GribanovDaniil@users.noreply.github.com>
2022-11-27 14:27:25 +0300

    Add files via upload

---
+++ Inheritance/Inheritance 4.py
@@ -0,0 +1,21 @@
+# -*- coding: windows-1251 -*-
+class Work():
+    def __init__(self, place):
+        self.place = place
+
+    def workinfo(self):
+        print("Место работы: ", self.place)
+
+class WorkingStudent(Work):
+    def __init__(self,place, name, study):
+        super().__init__(place)
+        self.name = name
+        self.study = study
+
+    def print_info(self):
+        print("Имя: ", self.name)
+        print("Место учебы: ", self.study)
+        self.workinfo()
+
+Daniil = WorkingStudent("Повариха в столовой", "Катюха", "Сириус" )
+Daniil.print_info()
