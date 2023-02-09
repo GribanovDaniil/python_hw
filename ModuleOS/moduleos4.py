3f0b33d408b7997e425da460ea65425949270beb
GribanovDaniil <113581434+GribanovDaniil@users.noreply.github.com>
2023-02-09 18:45:57 +0300

    Add files via upload

---
+++ ModuleOS/moduleos4.py
@@ -0,0 +1,10 @@
+# -*- coding: windows-1251 -*-
+""" напишите программу которая создает папку task4 и записывает текст "я выполнил задание" в файл answer.txt
+"""
+
+
+import os
+os.mkdir(r'C:\task4')
+os.chdir(r'C:\task4')
+with open('answer.txt', 'w') as f:
+    f.write('я выполнил задание ')
